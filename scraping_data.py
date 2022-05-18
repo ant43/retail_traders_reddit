6f9cc00939009026284dec087244958f2ea2252c
ant43 <ant43@case.edu>
2022-05-17 23:35:20 -0400

    this is to add all the things that work and so i can start to work on this on my laptop

--- scraping_data.py
+++ scraping_data.py
@@ -7,6 +7,11 @@ import datetime as dt
 #Now we will acces the reddit app
 
 
+
+
+
+#def getDataAboutStock(stock, date, stock)
+
 pushShiftAPI = PushshiftAPI()
 
 #this is to test it so I am not spending a lot of time doing these things
@@ -15,7 +20,8 @@ after = int(dt.datetime(2021,2,1,0,0).timestamp())
 
 
 subreddit="wallstreetbets"
-limit=100000
-comments = pushShiftAPI.search_comments(q = 'GME',fields = ['author', 'body', ],subreddit=subreddit, before=before, after=after)
+comments = pushShiftAPI.search_comments(q = 'GME',fields = ['author', 'body', 'created_utc'],subreddit=subreddit, before=before, after=after)
 comments_df = pd.DataFrame(comments)
-comments_df.to_csv('./wsb_comments.csv', header=True, index=False, columns=list(comments_df.axes[1]))
+comments_df['created_utc'] = comments_df['created_utc'].transform(dt.datetime.fromtimestamp)
+comments_df['stock'] = 'GME'
+comments_df.to_csv('./'+'wsb_comments'+'.csv', header=True, index=False, columns=list(comments_df.axes[1]))
