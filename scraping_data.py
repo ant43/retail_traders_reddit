cf9faa81d4002062556569f4e87bf215a588e9cb
ant43 <ant43@case.edu>
2022-05-17 22:31:44 -0400

    basicaly the thing works for looking at somthing with gme

--- scraping_data.py
+++ scraping_data.py
@@ -1,5 +1,6 @@
 #use pushscrap to get a lot of data off of reddit
 import csv
+import pandas as pd
 from pmaw import PushshiftAPI
 #becuae the API works with datetime I will have to import another package to deal with this
 import datetime as dt
@@ -9,12 +10,12 @@ import datetime as dt
 pushShiftAPI = PushshiftAPI()
 
 #this is to test it so I am not spending a lot of time doing these things
-before = int(dt.datetime(2021,2,1,0,0).timestamp())
-after = int(dt.datetime(2020,12,1,0,0).timestamp())
+before = int(dt.datetime(2021,2,1,0,1).timestamp())
+after = int(dt.datetime(2021,2,1,0,0).timestamp())
 
 
 subreddit="wallstreetbets"
 limit=100000
-comments = pushShiftAPI.search_comments(subreddit=subreddit, limit=limit, before=before, after=after)
-print(f'Retrieved {len(comments)} comments from Pushshift')
-print(comments)
+comments = pushShiftAPI.search_comments(q = 'GME',fields = ['author', 'body', ],subreddit=subreddit, before=before, after=after)
+comments_df = pd.DataFrame(comments)
+comments_df.to_csv('./wsb_comments.csv', header=True, index=False, columns=list(comments_df.axes[1]))
