a83c583ffca52fa0cb2aadc4974ea0e2be4b60c5
ant43 <ant43@case.edu>
2022-05-17 21:36:18 -0400

    i found out i have too much data to use praw so now i have to use a diffrent package called pushshift API

--- scraping_data.py
+++ scraping_data.py
@@ -1,10 +1,3 @@
 #will be using praw to scrap information off of reddit
-import praw
-import pandas as pd
+from pmaw import PushshiftAPI
 #Now we will acces the reddit app
-
-reddit = praw.Reddit(client_id='5QEZMjC8rrhrCWZPvf3hrg', client_secret='JmmRYmSROtAZMS0SpdempGzi9yyFSw', user_agent='tati_work_web')
-
-hot_posts = reddit.subreddit('all').hot(limit=10)
-for post in hot_posts:
-    print(post.title)
