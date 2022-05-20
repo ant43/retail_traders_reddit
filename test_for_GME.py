b95acbf1f623811bb98a24ecc3f74b9cedaefc42
ant43 <ant43@case.edu>
2022-05-20 00:00:02 -0400

    fixed some errors that i found

--- test_for_GME.py
+++ test_for_GME.py
@@ -1,4 +1,5 @@
 import get_diffrent_day as gbd
+import datetime as dt
 
 gbd.get_data_about_stock(dt.datetime(2020, 12, 13, 0, 0).timestamp(), 86400, 'GME')
 gbd.get_data_about_stock(dt.datetime(2020, 12, 13, 0, 0).timestamp(), 86400*7, 'GME')
