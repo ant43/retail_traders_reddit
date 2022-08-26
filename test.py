60ed7f9c062965241721cc8995480a41252e4752
Andrei_Tiu <andrei@desktop-f6p4mji.lan>
2022-08-26 19:17:39 -0400

    finaly fixed the errors with the ints representing the dates

--- test.py
+++ test.py
@@ -1,30 +1,6 @@
-def _strGreaterThan(string1: str, string2: str) -> bool:
-    l = [string1, string2]
-    l.sort()
-    if l[0] == string1:
-        return True
-    else:
-        return False
+import collecting_ticker_data_retail_reddit as cr
+import datetime as df
 
 
-def testBinarySearch (stringList, target, begining , end ):
-    if begining <= end:
-        middle = (begining+end) // 2
-        print(middle)
-        if stringList[middle] == target:
-            return True
-        elif _strGreaterThan(stringList[middle], target):
-            return testBinarySearch(stringList, target, middle+1, end)
-        elif _strGreaterThan(target, stringList[middle]):
-            return testBinarySearch(stringList, target, begining, middle-1)
-    else:
-        return False
-
-L = ['thing', 'fuck', 'whatever', 'horrible', 'how_is_this_possible']
-
-L.sort()
-print(L)
-#print (testBinarySearch(L, 'whatever', 0, len(L)+1))
-#print (testBinarySearch(L, 'horrible', 0, len(L)+1))
-#print('got this far')
-print(testBinarySearch(L, 'anything', 0, len(L)+1))
+cr.collect_retail_data_csv(year = 2020, month = 12, day =13, ticker_list = ['csv'],
+ csv_file_name = 'testCSV', time = 86400, subreddit = 'wallstreetbets')
